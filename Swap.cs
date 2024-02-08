d4e8c8ca189299073893042e04303c7d5317e7cb
Anandha Padmanabhan <anandhapadmanabhan@Anandhas-MacBook-Air.local>
2024-02-07 20:26:58 -0500

    3rd program Swap.cs done

---
+++ Swap.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Swap
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter x:");
+        int x = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("Enter y:");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Before swapping: x: {x}; x: {y};");
+
+        // Swapping the values using a temporary variable
+        int temp = x;
+        x = y;
+        y = temp;
+
+        Console.WriteLine($"After swapping: y: {x}; y: {y};");
+    }
+}
